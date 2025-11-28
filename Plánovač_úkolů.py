17bd5e9f55e191aa3a1ebe459db329b8da605af2
MatejStebel <stebel.matej@gmail.com>
2025-11-28 14:02:04 +0100

    funkční je jenom python soubor

---
+++ Plánovač_úkolů.py
@@ -0,0 +1,54 @@
+úkoly = []
+print("*******************************")
+print("\nVítejte v plánovači úkolů!\n")
+print("*******************************")
+
+def přidat_úkol():
+    název = input("Zadejte název úkolu: ")
+    předmět = input("Zadejte předmět: ")
+    datum_odevzdání = input("Zadejte datum odevzdání (DD-MM-RRRR): ")
+    úkoly.append({"název": název, "předmět": předmět, "datum_odevzdání": datum_odevzdání})
+    print("Úkol přidán.")
+
+def zobrazit_úkoly():
+    if not úkoly:
+        print("\nŽádné úkoly nejsou zadány.")
+        return
+    for i, úkol in enumerate(úkoly):
+        print(f"\nČíslo úkolu: {i}\n      Název: {úkol['název']}\n      Předmět: {úkol['předmět']}\n      Datum odevzdání: {úkol['datum_odevzdání']}")
+
+def odstranit_úkol():
+    zobrazit_úkoly()
+    index = int(input("\nZadejte číslo úkolu k odstranění: "))
+    if 0 <= index < len(úkoly):
+        úkoly.pop(index)
+        print("\nÚkol odstraněn.")
+    else:
+        print("\nNeplatné číslo úkolu.")
+
+def konec():
+    print("\nProgram ukončen.")
+    exit()
+
+def hlavní_menu():
+    print("\nvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv")
+    print("\nVitejte v hlavním menu.\n")
+    print("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\n")
+    print("1. Přidat úkol")
+    print("2. Zobrazit úkoly")
+    print("3. Odstranit úkol")
+    print("4. Konec")
+    volba = input("\nZadejte číslo akce, kterou chcete provést: ")
+    if volba == "1":
+        přidat_úkol()
+    elif volba == "2":
+        zobrazit_úkoly()
+    elif volba == "3":
+        odstranit_úkol()
+    elif volba == "4":
+        konec()
+    else:
+        print("\nŠpatné číslo, zadejte jiné číslo.")
+        hlavní_menu()
+while True:
+    hlavní_menu()
